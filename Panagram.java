6d6541677bafb21bad5e071690fc0044c8a55e12
Tousifulla H <129282402+Tousif-hafiz@users.noreply.github.com>
2023-10-11 23:47:55 +0530

    Update Panagram.java

--- Panagram.java
+++ Panagram.java
@@ -9,17 +9,31 @@ public static void main(String[] args)
     System.out.println("Enter the String to check Panagram or not");
     String str=Keybord.nextLine();
     str=str.toLowerCase();
-    TreeSet<Character> ts=new TreeSet<Character>();
-    for(int i=0;i<str.length();i++)
-    {
-        if(str.charAt(i)!=' ')
-        {
-            ts.add(str.charAt(i));
+    // TreeSet<Character> ts=new TreeSet<Character>();
+    // for(int i=0;i<str.length();i++)
+    // {
+    //     if(str.charAt(i)!=' ')
+    //     {
+    //         ts.add(str.charAt(i));
+    //     }
+    // }
+    // if(ts.size()==26)
+    //     System.out.println("Its a Panagram");
+    // else
+    //     System.out.println("Its not a Panagram");
+    // }
+    /*C. Check if the input is pangram or not. (Pangram is a sentence that contains all the 
+alphabets from a-z */
+    boolean allLetter=true;
+    for(char c='a';c<='z';c++){
+        if(!str.contains(String.valueOf(c))){
+            allLetter=false;
+            break;
         }
     }
-    if(ts.size()==26)
+    if(allLetter)
         System.out.println("Its a Panagram");
     else
         System.out.println("Its not a Panagram");
-    }
+}
 }
