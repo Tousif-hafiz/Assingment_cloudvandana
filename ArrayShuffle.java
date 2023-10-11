bc16163d161d3181ca92ab90c2df93f2cce9bf7d
Tousifulla H <129282402+Tousif-hafiz@users.noreply.github.com>
2023-10-11 23:06:26 +0530

    Update ArrayShuffle.java

--- ArrayShuffle.java
+++ ArrayShuffle.java
@@ -6,7 +6,7 @@ public class ArrayShuffle {
     public static void main(String[] args)
     {
         int[] array= {1,2,3,4,5,6,7};
-        System.out.println("Original Array: "+Arrays.toString(array));
+        System.out.println("Original Array: "+toString(array));
         for(int i=array.length-1;i>=0;i--)
         {
             Random r=new Random();
@@ -15,6 +15,20 @@ public class ArrayShuffle {
             array[i]=array[j];
             array[j]=temp;
         }
-        System.out.println("Suffled Array: "+Arrays.toString(array));
+        System.out.println("Suffled Array: "+toString(array));
+    }
+    // toString method is developed to print like a collection
+    static String toString(int[] array){
+        StringBuilder str=new StringBuilder("[");
+        for(int i=0;i<array.length-1;i++){
+            if(i!=array.length-1)
+            {
+                str.append(array[i]);
+                str.append(",");
+            }
+            str.append(array[array.length-1]);
+        }
+        str.append("]");
+        return str.toString();
     }
 }
